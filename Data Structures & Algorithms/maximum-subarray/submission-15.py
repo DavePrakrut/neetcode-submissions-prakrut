408b7bc8d02f1f1f05e8ba0963d8c94e6ab4ddb6
Prakrut Dave <daveprakrut@gmail.com>
2026-05-01 05:00:18 +0530

    Add: maximum-subarray - submission-15

---
+++ Data Structures & Algorithms/maximum-subarray/submission-15.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        c=m=nums[0]
+        for i in nums[1:]:
+            c=max(i,i+c)
+            m=max(m,c)
+        return m    
+        
